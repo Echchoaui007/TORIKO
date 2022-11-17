c6a37d250fa3c44dd9545e4397cb769d01da1d2b
Echchaoui <kurra9904@gmail.com>
2022-11-17 11:08:29 +0100

    zed

--- javascript/filter.js
+++ javascript/filter.js
@@ -6,7 +6,6 @@ list.forEach((el) => {
         list.forEach((li) => {
             li.classList.remove("active")
         })
-
         e.target.classList.add("active");
         boxes.forEach((el2) => {
             el2.style.display = "none";
